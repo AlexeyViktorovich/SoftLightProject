ee67b768052e4635969e6619545508f1916139cd
Alexey <apolin87@yandex.ru>
2021-09-30 02:52:29 +0500

    first

---
+++ SoftLightProject/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SoftLightProject
+//
+//  Created by Алексей Полин on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
